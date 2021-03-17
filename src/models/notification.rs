d7ab90ef2ad1b2d30394541a0712a4ff4b05d34b
xrei <romanrei.dev@gmail.com>
2021-03-17 20:50:49 +0300

    add create notification

--- src/models/notification.rs
+++ src/models/notification.rs
@@ -1,10 +1,11 @@
 use crate::schema::notifications;
 use crate::schema::notifications::dsl::*;
-use diesel::dsl::not;
 use serde::{Deserialize, Serialize};
 use actix_web::{web, Error, HttpResponse};
 use crate::Pool;
-use crate::diesel::{QueryDsl, RunQueryDsl, result::Error as DieselError};
+use crate::diesel::{QueryDsl, RunQueryDsl, result::Error as DieselError, insert_into};
+
+type DbPool = web::Data<Pool>;
 
 #[derive(Queryable)]
 pub struct Notification {
@@ -15,20 +16,30 @@ pub struct Notification {
 #[derive(Insertable)]
 #[table_name="notifications"]
 pub struct NewNotification {
-  pub price: Option<f64>,
+  pub price: f64,
 }
 
 impl Notification {
-  pub async fn find_all(pool: web::Data<Pool>) -> Result<Vec<Notification>, DieselError> {
+  pub async fn find_all(pool: DbPool) -> Result<Vec<Notification>, DieselError> {
     let conn = pool.get().unwrap();
     let items = notifications.load::<Notification>(&conn)?;
     Ok(items)
   }
 
-  pub async fn find_by_id(nid: i32, pool: web::Data<Pool>) -> Result<Notification, DieselError> {
+  pub async fn find_by_id(nid: i32, pool: DbPool) -> Result<Notification, DieselError> {
     let conn = pool.get().unwrap();
     let notification = notifications::table.find(nid).first(&conn)?;
 
     Ok(notification)
+  }
+
+  pub async fn create(notification: web::Json<NewNotification>, pool: DbPool) -> Result<Notification, DieselError> {
+    let conn = pool.get().unwrap();
+    let new_notification = NewNotification {
+      price: notification.price,
+    };
+    let res = insert_into(notifications::table).values(new_notification).get_result(&conn)?;
+
+    Ok(res)
   }
 }
